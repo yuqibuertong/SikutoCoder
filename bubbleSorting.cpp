65f5786a9e407d83d48b2cd1b058bb83f380beb5
Sikuto <34903165+yuqibuertong@users.noreply.github.com>
2018-12-22 00:01:46 +0800

    Add files via upload

---
+++ bubbleSorting.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n, i, j;
+	int a[10000];
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) scanf("%d", &a[i]);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n - 1 - i; j++) {
+			if (a[j + 1] < a[j]) swap(a[j + 1], a[j]);
+		} 
+	}
+	for (i = 0; i < n; i++) printf("%d ", a[i]);
+	return 0;
+}
