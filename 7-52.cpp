f77ac125af1a6019e3be4b2f48f32a23dae2d50e
Sikuto <34903165+yuqibuertong@users.noreply.github.com>
2018-12-29 20:38:44 +0800

    Add files via upload

---
+++ 7-52.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct node{
+	int num;
+	struct node* next;	
+}l;
+int main() {
+	l* a, *b, *c;
+	int k = 0;
+	int i;
+	a = new node;
+	a->next = NULL;
+	c = a;
+	while (1) {
+		cin >> i;
+		if (i == -1) break;
+		c->next = new node;
+		c = c->next;
+		c->next = NULL;
+		c->num = i;
+	}
+	b = new node;
+	b->next = NULL;
+	c = b;
+	while (1) {
+		cin >> i;
+		if (i == -1) break;
+		c->next = new node;
+		c = c->next;
+		c->next = NULL;
+		c->num = i;
+	}
+	a = a->next;
+	b = b->next;
+	while (a !=NULL && b != NULL) {
+		if (a->num < b->num) a = a->next;
+		else if (a->num > b->num) b = b->next;
+		else {
+			if (k) cout << " " << b->num;
+			else {
+				k++;
+				cout << b->num;
+			}
+			b = b->next;
+			a = a->next;
+		}
+	}
+	if (!k) cout << "NULL";
+	return 0;
+}
