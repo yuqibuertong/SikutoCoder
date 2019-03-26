94838b4205d2401d5ecdcb12ede0de3b2b2ee19f
Sikuto <34903165+yuqibuertong@users.noreply.github.com>
2019-03-26 10:19:50 +0800

    Create 1163.Tour.cpp

---
+++ 1163.Tour.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 50;
+int n, x[maxn], y[maxn];
+double d[maxn][maxn], f[maxn][maxn];
+
+double num() {
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			d[i][j] = hypot(x[i] - x[j], y[i] - y[j]);
+	f[0][1] = d[0][1];
+	for (i = 2; i < n; i++) {
+		for (j = 0; j < i - 1; j++)
+			f[j][i] = f[j][i - 1] + d[i - 1][i];
+		f[i - 1][i] = 99999999;
+		for (j = 0; j < i - 1; j++)
+			f[i - 1][i] = min(f[i - 1][i], f[j][i - 1] + d[j][i]);	
+	}
+	return f[n - 2][n - 1] + d[n - 2][n - 1]; 
+}
+
+int main() {
+	int i, j;
+	while (scanf("%d", &n) != EOF) {
+		for (i = 0; i < n; i++) scanf("%d %d", &x[i], &y[i]);
+		printf("%.2lf\n", num());
+	}
+	return 0;
+}
