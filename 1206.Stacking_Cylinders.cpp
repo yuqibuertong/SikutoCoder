7875b13f9d7aec81bf138083372528124635fcfa
Sikuto <34903165+yuqibuertong@users.noreply.github.com>
2019-04-14 20:50:53 +0800

    Create 1206.Stacking_Cylinders.cpp

---
+++ 1206.Stacking_Cylinders.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, n, i, j, k;
+	scanf("%d", &a);
+	for (k = 1; k <= a; k++) {
+		scanf("%d", &n);
+		vector<pair<double, double> > d(n * (n + 1) / 2);
+		for (i = 0; i < n; i++) scanf("%lf", &d[i].first);
+		for (i = 0; i < n; i++) d[i].second = 1;
+		sort(d.begin(), d.begin() + n);
+		while (n > 1) {
+			for (j = 0; j < n - 1; j++) {
+				double t1 = d[i - n + 1].first - d[i - n].first,
+				t2 = d[i - n + 1].second - d[i - n].second,
+				a1 = atan(t2 / t1), a2 = acos(sqrt(t1 * t1 + t2 * t2) / 4),
+				a = a1 + a2;
+				d[i].first = d[i - n].first + 2 * cos(a);
+				d[i].second = d[i - n].second + 2 * sin(a);
+				++i;
+			}
+			--n;
+		}
+		printf("%d: %.4f %.4f\n", k, d[d.size() - 1].first, d[d.size() - 1].second);
+	}
+	return 0;
+}
